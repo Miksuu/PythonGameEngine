be48832d59ae1e20055ca38f0558a3b05a4c00ea
Miksuu <imiksuu96@gmail.com>
2023-09-26 13:30:45 +0300

    Add windowManagement class

--- PythonGameEngine/PythonGameEngine.py
+++ PythonGameEngine/PythonGameEngine.py
@@ -8,8 +8,10 @@ from GameObject import GameObject
 from TriangleGameObject import TriangleGameObject
 from GameObjectManager import GameObjectManager
 from Vector2 import Vector2
+from WindowManagement import WindowManagement
 
 gameObjectManager = GameObjectManager()
+windowManagement = WindowManagement(gameObjectManager)
 
 camera = Camera()
 
@@ -24,14 +26,7 @@ draggingMouse = False
 
 def main():
     print("Starting...")
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
-    glutInitWindowSize(1200,1200)
-    glutInitWindowPosition(50,50)
-    glutCreateWindow(b'test')
-    glutDisplayFunc(plotpoints)
-    glClearColor(0.0,0.0,0.0,1.0)
-    gluOrtho2D(-1.0,1.0,-1.0,1.0)
+    windowManagement.setupWindow()
     
     glutKeyboardFunc(keyboard)
     
@@ -71,13 +66,6 @@ def mouseDrag(x, y):
 
 def recenterCamera():
     camera.position = player.position
-
-def plotpoints():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    gameObjectManager.handleGameLoop()
-    glutSwapBuffers()
-    glFlush() 
 
 def keyboard(key, x, y):
     player.inputManager.move(key)
