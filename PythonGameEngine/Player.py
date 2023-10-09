5d249e63ef72fd7f044382fe88390513ce13650a
Miksuu <imiksuu96@gmail.com>
2023-10-09 11:58:13 +0300

    Updated player class

--- PythonGameEngine/Player.py
+++ PythonGameEngine/Player.py
@@ -9,7 +9,17 @@ from InputManager import InputManager
 class Player(GameObject):
     def __init__(self, name, position):
         super().__init__(name, position)
+        self.movementSpeed = 1.0
         
         self.camera = Camera()
         self.inputManager = InputManager(self, self.camera)
         self.renderer = Renderer(self.camera, name)
+
+    def move(self, direction):
+        self.velocity = direction * self.movementSpeed
+
+    def update(self):
+        self.position += self.velocity
+
+    def __repr__(self):
+        return f"Player(Position: {self.position}, Velocity: {self.velocity}, MovementSpeed: {self.movementSpeed})"
