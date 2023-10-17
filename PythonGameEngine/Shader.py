793dc94610e9ebd87ab989d33a4e0f35a11f4fdb
Miksuu <imiksuu96@gmail.com>
2023-10-17 13:51:02 +0300

    Loading some texture

--- PythonGameEngine/Shader.py
+++ PythonGameEngine/Shader.py
@@ -1,13 +1,16 @@
 from OpenGL.GL import *
 from FileManager import FileManager
-
 import ctypes
+import numpy as np
+from PIL import Image
 
 class Shader:
     def __init__(self, assetName):
         self.assetName = assetName
-        # self.color = (0,0,0)
         self.shader = self.initialize()
+
+        self.initTextures()
+
         glUseProgram(self.shader)
 
     def initialize(self):
@@ -41,6 +44,26 @@ class Shader:
         
         return shader
     
+    def initTextures(self):
+        texturePath = "Assets/Textures/wall.png"
+
+        texture = self.loadTexture(texturePath)
+        glBindTexture(GL_TEXTURE_2D, texture)
+
+        #glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), ctypes.c_void_p(0))
+        #glEnableVertexAttribArray(0)
+        #glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), ctypes.c_void_p(12))
+        #glEnableVertexAttribArray(1)
+
+    def loadTexture(self, path):
+        image = Image.open(path)
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+        img_data = np.array(list(image.getdata()), np.uint8)
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+        return texture
+
     # def setShaderUniforms(self):
     #     colorLocation = glGetUniformLocation(self.shader, "objectColor")
     #     glUseProgram(self.shader)
